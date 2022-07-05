042421acd506891c3d364ea3b19ed65d0978243d
CalebOkyere <agyakojo4321@gmail.com>
2022-07-05 22:42:45 +0000

    post graphql type

--- graphql/post.js
+++ graphql/post.js
@@ -1,7 +1,49 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
+import { UserModel } from "../model";
+import UserType from "./user";
+
+const CommentType = new GraphQLObjectType({
+  name: "Comment",
+  fields: {
+    body: {
+      type: GraphQLString,
+    },
+    user: {
+      type: UserType,
+      resolve: async (parent, args) => {
+        const user = await UserModel.findById(parent.user);
+        return user;
+      },
+    },
+    date: {
+      type: GraphQLString,
+    },
+  },
+});
 
 const PostType = new GraphQLObjectType({
   name: "Post",
+  fields: {
+    description: {
+      type: GraphQLString,
+    },
+    image: {
+      type: GraphQLString,
+    },
+    like: {
+      type: new GraphQLList(UserType),
+      resolve: async (parent, args) => {
+        const likes = Array(parent.like).map(async (element) => {
+          return await UserModel.findOne({ _id: element });
+        });
+
+        return likes;
+      },
+    },
+    comment: {
+      type: new GraphQLList(CommentType),
+    },
+  },
 });
 
 export default PostType;
