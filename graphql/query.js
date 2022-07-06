3b40a60663f6e71707ede203d66f61c0bf84fde7
CalebOkyere <agyakojo4321@gmail.com>
2022-07-06 12:32:26 +0000

    necessary queries done

--- graphql/query.js
+++ graphql/query.js
@@ -1,5 +1,6 @@
-import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from "graphql";
-import { UserModel } from "../model";
+import { GraphQLList, GraphQLObjectType } from "graphql";
+import { PostModel, UserModel } from "../model";
+import PostType from "./post";
 import UserType from "./user";
 
 const query = new GraphQLObjectType({
@@ -17,6 +18,45 @@ const query = new GraphQLObjectType({
       resolve: async () => {
         const users = await UserModel.find();
         return users;
+      },
+    },
+    getPost: {
+      type: PostType,
+      resolve: async (parent, args) => {
+        const post = await PostModel.findOne({ _id: args.postId });
+        return post;
+      },
+    },
+    getPosts: {
+      type: new GraphQLList(PostType),
+      resolve: async () => {
+        const posts = await PostModel.find();
+        return posts;
+      },
+    },
+    getUserPosts: {
+      type: new GraphQLList(PostType),
+      resolve: async (parent, args) => {
+        const user = await UserModel.findOne({ _id: args.userId }).populate(
+          "posts"
+        );
+        const userPosts = user.posts;
+        return userPosts;
+      },
+    },
+    getFollowingPosts: {
+      type: new GraphQLList(PostType),
+      resolve: async (parent, args, { _user }) => {
+        const post = [];
+        const user = await UserModel.findOne({ _id: _user });
+        const following = user.following;
+        Array(following).forEach(async (element) => {
+          let userPost = await PostModel.find({ user: element });
+          if (userPost) {
+            post.concat(userPost);
+          }
+        });
+        return post;
       },
     },
   },
--- model/post.js
+++ model/post.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
   description: {
     type: mongoose.SchemaTypes.String,
   },
